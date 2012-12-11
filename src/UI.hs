bdd4160798b6aaefc26f2f2213e2efa141a25c47
Maciek Makowski <maciek.makowski@gmail.com>
2012-12-11 20:06:58 +0000

    UI skeleton

---
+++ src/UI.hs
@@ -0,0 +1,17 @@
+module UI where
+
+
+renderSvg :: [BotState] -> [Bullet] -> Svg 
+renderSvg = undefined
+
+renderBot :: BotState -> Svg
+renderBot = undefined
+
+renderBullet :: Bullet -> Svg
+renderBullet = undefined
+
+server :: IO ()
+server = undefined 
+
+javascript :: String
+javascript = undefined
